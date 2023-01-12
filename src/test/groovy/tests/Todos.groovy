5efbecaca80c8f68d8c167f340e27218030445d0
Fran Fernandez <zoomff@gmail.com>
2023-01-12 15:58:27 -0300

    Adding Allure reports

--- src/test/groovy/tests/Todos.groovy
+++ src/test/groovy/tests/Todos.groovy
@@ -1,15 +1,22 @@
 package tests
 
 import common.Endpoints
+import io.qameta.allure.Description
+import io.qameta.allure.Feature
+import io.qameta.allure.Severity
+import io.qameta.allure.SeverityLevel
 import io.restassured.module.jsv.JsonSchemaValidator
 import io.restassured.response.Response
 import org.testng.Assert
 import org.testng.annotations.Test
 import static io.restassured.RestAssured.*
 
+@Feature("Todos")
 class Todos extends Base {
 
-    @Test(description = "Verify that /todos is returning a 200 response", priority = 0)
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "Verify that /todos is returning a 200 response", groups = "smoke", priority = 0)
+    @Description("Does a get to /todos and asserts a 200 response with a correct schema response")
     void checkTodosReturns200() {
         Response response = get(Endpoints.getEndpoint("todos"))
         int responseCode = response.then().extract().statusCode()
@@ -18,7 +25,9 @@ class Todos extends Base {
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/" + "todos-200.json"))
     }
 
-    @Test(description = "Verify that /todos is returning the correct amount of items", priority = 1)
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify that /todos is returning the correct amount of items", groups = "regression", priority = 1)
+    @Description("Does a get to /todos and asserts that 200 items are returned")
     void checkTodosReturnsCorrectAmountOfItems() {
         int expected = 200
         Response response = get(Endpoints.getEndpoint("todos"))
